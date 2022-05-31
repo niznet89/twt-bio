25a5455286793d074f29e8289a75a23aa9061765
Tenzin Rose <tenzinrose@gmail.com>
2022-05-31 14:30:55 -0700

    Added validation / hint for Project description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true
+  validates :description, length: { maximum: 200,
+    too_long: "200 characters is the maximum allowed" }
   belongs_to :user
 end
