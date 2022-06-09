818a5bc3c6bdb361d9fbdccc2b2b746ae64c0af7
Josh Merrill <joshmmerrill@outlook.com>
2022-06-08 17:41:29 -0700

    STYLE: Updated sign-up page image w / new placeholder and changed the image avatar styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def profile_photo(user)
     if user.photo.attached?
-      cl_image_tag user.photo.key, :width=>300, :height=>300, :border=> { :width => 2, :color => '#303030' }, :gravity=>"face", :radius=>"max", :crop=>"fill", :format=> "png"
+      cl_image_tag user.photo.key, :class=> 'avatar-large', :border=> { :width => 2, :color => '#303030' }, :gravity=>"face", :radius=>"max", :crop=>"fill", :format=> "png"
     else
       image_tag "https://res.cloudinary.com/dmty5wfjh/image/upload/v1653601128/astronaut_tykby8.jpg", class: "avatar-large"
     end
